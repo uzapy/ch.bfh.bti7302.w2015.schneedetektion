ecc77ff0ece30da201b1451e11aa4948130b1d64
uzapy <uzapy@outlook.com>
2015-09-30 21:18:46 +0200

    Bilder in Unterordner verschieben. Ein Unterordner pro Tag.

--- Schneedetektion/Schneedetektion.ImageMove/Program.cs
+++ Schneedetektion/Schneedetektion.ImageMove/Program.cs
@@ -1,33 +1,69 @@
-﻿using System;
+﻿using Schneedetektion.ImageMove.Properties;
+using System;
 using System.IO;
 
 namespace Schneedetektion.FileMove
 {
     public class Program
     {
-        private static string folderName = @"C:\Users\uzapy\Desktop\astra\mvk131";
-        private static string folderNameLog = @"C:\Users\uzapy\Desktop\astra\log_mvk131";
-        private static string fileName = String.Empty;
-        private static string[] fileNames;
+	   /* Bilder in Unterordner verschieben (Unterordner nach Tag) */
+	   private static string folderName = Settings.Default.WorkingFolder;
+	   private static string[] cameraNames = { "mvk021", "mvk101", "mvk105", "mvk107", "mvk110", "mvk120", "mvk122", "mvk131" };
+	   private static string[] fileNames;
 
-        static void Main(string[] args)
-        {
-            if (!Directory.Exists(folderNameLog))
-            {
-                Directory.CreateDirectory(folderNameLog);
-            }
+	   static void Main(string[] args)
+	   {
+		  foreach (string cameraName in cameraNames)
+		  {
+			 string folder = folderName + "\\" + cameraName;
+			 fileNames = Directory.GetFiles(folder);
 
-            fileNames = Directory.GetFiles(folderName);
-            for (int i = 0; i < fileNames.Length-1; i++)
-            {
-                if (fileNames[i].EndsWith(".log"))
-                {
-                    fileName = Path.GetFileName(fileNames[i]);
-                    File.Move(fileNames[i], folderNameLog + "\\" + fileName);
-                    Console.WriteLine(folderNameLog + "\\" + fileName);
-                }
-            }
-            Console.WriteLine("Finished!");
-        }
+			 foreach (string file in fileNames)
+			 {
+				string fileName = Path.GetFileNameWithoutExtension(file);
+				string subFolder = folderName + "\\" +  cameraName + "\\" + fileName.Substring(7, 8);
+
+				if (!Directory.Exists(subFolder))
+				{
+				    Directory.CreateDirectory(subFolder);
+				}
+
+				File.Move(file, subFolder + "\\" + Path.GetFileName(file));
+				Console.WriteLine(subFolder + "\\" + Path.GetFileName(file));
+
+			 }
+
+			 Console.WriteLine(cameraName);
+		  }
+		  Console.WriteLine("Finished!");
+		  Console.ReadLine();
+	   }
+
+	   /* Bilder und Log-Dateien trennen */
+	   //private static string folderName = @"C:\Users\uzapy\Desktop\astra";
+	   //private static string folderNameLog = @"C:\Users\uzapy\Desktop\astra\log_mvk131";
+	   //private static string fileName = String.Empty;
+	   //private static string[] fileNames;
+
+	   //static void Main(string[] args)
+	   //{
+	   //    if (!Directory.Exists(folderNameLog))
+	   //    {
+	   //        Directory.CreateDirectory(folderNameLog);
+	   //    }
+
+	   //    fileNames = Directory.GetFiles(folderName);
+	   //    for (int i = 0; i < fileNames.Length-1; i++)
+	   //    {
+	   //        if (fileNames[i].EndsWith(".log"))
+	   //        {
+	   //            fileName = Path.GetFileName(fileNames[i]);
+	   //            File.Move(fileNames[i], folderNameLog + "\\" + fileName);
+	   //            Console.WriteLine(folderNameLog + "\\" + fileName);
+	   //        }
+	   //    }
+	   //    Console.WriteLine("Finished!");
+	   //}
+
     }
 }

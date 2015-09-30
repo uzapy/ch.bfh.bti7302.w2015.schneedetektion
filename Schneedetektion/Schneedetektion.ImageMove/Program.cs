0604c03045079b7e48e370115aa0e4157c668f6e
uzapy <uzapy@outlook.com>
2015-09-30 08:09:45 +0200

    Programm um die Bilder in sinvoller Zeit zu verscheiben.

---
+++ Schneedetektion/Schneedetektion.ImageMove/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Schneedetektion.FileMove
+{
+    public class Program
+    {
+        private static string folderName = @"C:\Users\uzapy\Desktop\astra\mvk131";
+        private static string folderNameLog = @"C:\Users\uzapy\Desktop\astra\log_mvk131";
+        private static string fileName = String.Empty;
+        private static string[] fileNames;
+
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists(folderNameLog))
+            {
+                Directory.CreateDirectory(folderNameLog);
+            }
+
+            fileNames = Directory.GetFiles(folderName);
+            for (int i = 0; i < fileNames.Length-1; i++)
+            {
+                if (fileNames[i].EndsWith(".log"))
+                {
+                    fileName = Path.GetFileName(fileNames[i]);
+                    File.Move(fileNames[i], folderNameLog + "\\" + fileName);
+                    Console.WriteLine(folderNameLog + "\\" + fileName);
+                }
+            }
+            Console.WriteLine("Finished!");
+        }
+    }
+}

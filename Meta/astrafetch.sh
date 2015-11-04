adc93f4197a90b492cfd38be30ff7b7dc47a9fe7
uzapy <uzapy@outlook.com>
2015-11-04 22:44:51 +0100

    Added Folder with additional Files holding additional Information for the Project

---
+++ Meta/astrafetch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+url=http://www.astramobcam.ch/kamera
+dest=/srv/athena.bfh.ch/projects/astra/
+
+# dest directories need to exist!
+cams=(mvk021 mvk101 mvk105 mvk107 mvk110 mvk120 mvk122 mvk131)
+
+date=$(date -u +%Y%m%d_%H%M%S)
+echo $date
+
+if [ ! -d ${dest} ]; then
+	echo ${date} | mail -s "astrafetch - storage destination not available" vep2@bfh.ch -- -f vep2@bfh.ch
+	exit -1	
+fi
+
+for cam in ${cams[@]}; do
+	log=${dest}/${cam}/${cam}_${date}.log
+	img=${dest}/${cam}/${cam}_${date}.jpg
+	/usr/bin/wget ${url}/${cam}/live.jpg -O ${img} -o ${log}
+done
